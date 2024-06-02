4aa23fea533254eadf71a5e5f46d90c18bd4c81e
Justmiron007 <raen.96@mail.ru>
2024-06-02 21:47:16 +0500

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,9 @@
+immutable_var = 13, 'banana', 100, 'apple'
+print(immutable_var)
+#immutable_var [0] = 300
+#print(immutable_var)
+#нельзя изменить значения элементов кортежа!
+mutable_list = [13, 'banana', 100, 'apple']
+mutable_list [0] = 15
+mutable_list [3] = 'peach'
+print(mutable_list)
